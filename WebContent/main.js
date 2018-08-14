9aa8b3a7d357d11eb76cd774991e8cba3ef340e6
= <=>
2018-08-14 20:43:40 +0700

    Time5-Prototype

--- WebContent/main.js
+++ WebContent/main.js
@@ -2,6 +2,29 @@
 var danhSachSinhVien = new DanhSachSinhVien();
 
 var validate = new Validation();
+
+SinhVien.prototype.DiemToan='';
+SinhVien.prototype.DiemLy='';
+SinhVien.prototype.DiemHoa='';
+SinhVien.prototype.DTB='';
+SinhVien.prototype.Loai='';
+// Method
+SinhVien.prototype.TinhDTB= function(){
+	this.DTB = (Number(this.DiemToan) + Number(this.DiemLy) + Number(this.DiemHoa)) / 3;	
+}
+
+SinhVien.prototype.XepLoai = function(){
+	if(this.DTB <= 10 && this.DTB>=8){
+		this.Loai = "Xếp Lọai Giỏi";		
+	}else if(this.DTB <= 8 && this.DTB>=6.5){
+		this.Loai = "Xếp Lọai Khá";
+	}else if(this.DTB <= 6.5 && this.DTB>=5){
+		this.Loai = "Xếp Lọai Trung Bình";
+	}else{
+		this.Loai = "Xếp Lọai Yếu";
+	}
+}
+
 function ThemSinhVien(){
 	// get data user input.
 	var masv = document.getElementById("masv").value;
@@ -44,6 +67,11 @@ function ThemSinhVien(){
 	
 	// insert student
 	var sinhVien = new SinhVien(masv, hoten, email, sdt,cmnd);
+	sinhVien.DiemToan = document.getElementById("Toan").value;
+	sinhVien.DiemLy = document.getElementById("Ly").value;
+	sinhVien.DiemHoa = document.getElementById("Hoa").value;
+	sinhVien.TinhDTB();
+	sinhVien.XepLoai();
 	danhSachSinhVien.ThemSinhVien(sinhVien);
 	CapNhatDanhSachSV(danhSachSinhVien);
 	console.log(danhSachSinhVien);
@@ -77,7 +105,9 @@ function CapNhatDanhSachSV(DanhSachSinhVien){
 		var tdCMND = createTagTd("CMND", sv.CMND);
 		var tdEmail = createTagTd("Email", sv.Email);
 		var tdSDT = createTagTd("SoDT", sv.SoDT);
-		
+		//Tạo td  DTB và  xếp loại 
+        var tdDTB = createTagTd("DTB",sv.DTB);
+        var tdXepLoai = createTagTd ("XepLoai",sv.Loai);
 		// Append các td vào tr		
 		trSinhVien.appendChild(tdCheckBox);
 		trSinhVien.appendChild(tdMaSV);
@@ -85,6 +115,9 @@ function CapNhatDanhSachSV(DanhSachSinhVien){
 		trSinhVien.appendChild(tdEmail);
 		trSinhVien.appendChild(tdCMND);			
 		trSinhVien.appendChild(tdSDT);
+		trSinhVien.appendChild(tdDTB);
+        trSinhVien.appendChild(tdXepLoai);
+        
 		// Append tr vào tbodySinhVien
 		listTableSV.appendChild(trSinhVien);		 
 	}
@@ -128,9 +161,6 @@ function TimKiemSinhVien(){
 	CapNhatDanhSachSV(listDSSVTimKiem);
 }
 
-function changeColorDefaultTr(){
-	
-}
 function ChinhSuaSinhVien(masv){
 	var sinhVien = danhSachSinhVien.TimSVTheoMa(masv.trim());
 	if(sinhVien!=null){
@@ -149,19 +179,46 @@ function LuuThongTin()
     var cmnd = document.getElementById("cmnd").value;
     var email = document.getElementById("email").value;
     var sdt = document.getElementById("sdt").value;
-    var loi = 0;
-    //Kiểm tra validation    
-    if(loi != 0)
-    {
-        return ;
-    }
+    
+    // check validation.
+	if(validate.checkEmpty(masv)){
+		document.getElementById("masv").style.borderColor='red';
+		return;
+	}else{
+		document.getElementById("masv").style.borderColor='green';
+	}
+	if(validate.checkEmpty(hoten)){
+		document.getElementById("hoten").style.borderColor='red';
+		return;
+	}else{
+		document.getElementById("hoten").style.borderColor='green';
+	}
+	if(validate.checkEmpty(cmnd)){
+		document.getElementById("cmnd").style.borderColor='red';
+		return;
+	}else{
+		document.getElementById("cmnd").style.borderColor='green';
+	}
+	if(validate.checkEmail(email)){
+		document.getElementById("email").style.borderColor='green';
+	}else{
+		document.getElementById("email").style.borderColor='red';
+		return;
+	}
+	if(validate.checkNumberPhone(sdt)){
+		document.getElementById("sdt").style.borderColor='green';
+	}else{
+		document.getElementById("sdt").style.borderColor='red';
+		return;
+	}
+
     //Thêm sinh viên
     var sinhvien = new SinhVien(masv,hoten,email,sdt,cmnd);
-    /*sinhvien.DiemHoa = DomID("Toan").value;
-    sinhvien.DiemLy = DomID("Ly").value;
-    sinhvien.DiemHoa = DomID("Hoa").value;
+    sinhvien.Toan = document.getElementById("Toan").value;
+    sinhvien.DiemLy = document.getElementById("Ly").value;
+    sinhvien.DiemHoa = document.getElementById("Hoa").value;
     sinhvien.TinhDTB();
-    sinhvien.XepLoai();*/
+    sinhvien.XepLoai();
     danhSachSinhVien.SuaSinhVien(sinhvien);
     CapNhatDanhSachSV(danhSachSinhVien);
 }
